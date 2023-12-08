72074d8ae8dd83e52c46cc9a50ab47097e9201f1
Matheus Ribeiro Menezes <matheusribeiro.menezes@estudante.ufjf.br>
2023-12-08 15:41:05 -0300

    Criei funções Player 1 e 2 para definir quem joga e receber os valores dos dados

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 //main.js
 
 import { valorDado, RodadaDeQuem, AlteraRodada } from "./funções.js";
+
+let numSelec = null;
+
+Player1();
+
+function Player1()
+{
+    if(RodadaDeQuem())
+    {   
+        const dadoP1 = document.querySelector("#dadosP1");
+        let DadoP1 = valorDado();
+        dadoP1.textContent = DadoP1;
+        dadoP1.classList.add("numeros");
+        dadoP1.addEventListener("click", );
+    }
+}
+
+function Player2()
+{
+    if(!RodadaDeQuem())
+    {   
+        const dadoP2 = document.querySelector("#dadosP2");
+        let DadoP2 = valorDado();
+        dadoP2.textContent = DadoP2;
+        dadoP2.classList.add("numeros");
+        dadoP2.addEventListener("click", );
+    }
+}
