efcce2c72205379c8e2b333c8851fa6ef01f8940
Matheus Ribeiro Menezes <matheusribeiro.menezes@estudante.ufjf.br>
2023-12-08 16:14:21 -0300

    Criei função para mostrar quando o player 1 selecionou o dado

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function Player1()
         let DadoP1 = valorDado();
         dadoP1.textContent = DadoP1;
         dadoP1.classList.add("numeros");
-        dadoP1.addEventListener("click", );
+        dadoP1.addEventListener("click", SelecNum);
     }
 }
 
@@ -26,6 +26,11 @@ function Player2()
         let DadoP2 = valorDado();
         dadoP2.textContent = DadoP2;
         dadoP2.classList.add("numeros");
-        dadoP2.addEventListener("click", );
     }
+}
+
+function SelecNum()
+{
+    numSelec = this;
+    numSelec.classList.add("nselec");
 }
